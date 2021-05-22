17bfa9c6b6d3c86ef934accf0f0b3b0c17c3dec3
Ian <49589957+IWEZN@users.noreply.github.com>
2021-05-22 20:01:52 +0900

    Update buttonHover.cs

--- buttonHover.cs
+++ buttonHover.cs
@@ -5,36 +5,36 @@ public class buttonHover : MonoBehaviour
 {
     public int timeRemaining = 5;
     public GameObject ball;
-    
+
     void countDown()
     {
-        timeRemaining --;
-        if (timeRemaining <=0)
+        timeRemaining--;
+        if (timeRemaining <= 0)
         {
             playVideo();
-            
+
             //Reset timer
             CancelInvoke("countDown");
             timeRemaining = 5;
-            print ("Reset time");
-         ｝
-      ｝
+            print("Reset time");
+        }
+    }
       
       void playVideo()
-      {
-          print ("FINISHED COUNTDOWN");
-          Vector3 newpos = transform.position;
-          ball.transform.position = newpos;
-      }
-      
-      public void MouseOver()
-      {
-          InvokeRepeating ("countDown", 1, 1);
-      }
-      
-      public void MouseOut()
-      {
-          CancelInvoke ("countDown");
-          timeRemaining = 5;
-      }
-}
+            {
+                print("FINISHED COUNTDOWN");
+                Vector3 newpos = transform.position;
+                ball.transform.position = newpos;
+            }
+
+            public void MouseOver()
+            {
+                InvokeRepeating("countDown", 1, 1);
+            }
+
+            public void MouseOut()
+            {
+                CancelInvoke("countDown");
+                timeRemaining = 5;
+            }
+        }
